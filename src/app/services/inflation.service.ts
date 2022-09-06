9c43fd9ed79cb8621744973ee392ca93b6b2f9c6
tgbsrce <101837527+tgbsrce@users.noreply.github.com>
2022-09-06 14:24:34 +0300

    workshop

--- src/app/services/inflation.service.ts
+++ src/app/services/inflation.service.ts
@@ -7,21 +7,33 @@ import { BehaviorSubject, filter, repeat } from 'rxjs';
   providedIn: 'root'
 })
 export class InflationService {
+  variables =[
+   
+  ]
   private inflationRates = new BehaviorSubject<any>([]);
   inflationRates$ = this.inflationRates.asObservable();
 
   constructor(private http: HttpClient) { }
-  post: any;
+  post: any | undefined;
+  date:any;
   
-  getInflationRates(country="turkey",start= "",end="") {
+  getInflationRates(country="turkey",start="12.02.1998",end=`${this.date.end}`) {
 
     this.http.get('https://www.statbureau.org/get-data-json?country='+ country)
       .subscribe(response => {
-        this.inflationRates.next(response);
         this.post=response;
-        
-      
-        
+        // for (var i = 0; i < this.post.length; i++){
+        //   this.date=new Date(this.post[i].MonthFormatted);
+          
+        //   if(this.date>= start && this.date<=end){
+        //      this.inflationRates.next(response);
+        //   }
+           
+        // } 
+        const filterResponse=response.filter((data)=>{
+
+        })
+       
       })
       
   }
