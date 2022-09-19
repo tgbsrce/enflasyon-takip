094175c392e458d6cb126788f5748cb0c0ea5e6c
tgbsrce <101837527+tgbsrce@users.noreply.github.com>
2022-09-19 17:12:46 +0300

    added common module bug fixed

--- src/app/components/country-filter/country-filter.module.ts
+++ src/app/components/country-filter/country-filter.module.ts
@@ -1,18 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { CountryFilterComponent } from './country-filter.component';
 
-
-
 @NgModule({
-
-imports: [MatFormFieldModule,MatSelectModule,FormsModule],
-providers:  [],
-declarations: [CountryFilterComponent],
-exports:[CountryFilterComponent]
-
+  imports: [MatFormFieldModule, MatSelectModule, FormsModule, CommonModule],
+  providers: [],
+  declarations: [CountryFilterComponent],
+  exports: [CountryFilterComponent],
 })
-
-export class CountryFilterModule{}
+export class CountryFilterModule {}
