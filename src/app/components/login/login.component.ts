9734a25c06d95ddf1360f231e5893bdcf1ee22ab
tgbsrce <101837527+tgbsrce@users.noreply.github.com>
2022-10-04 11:06:00 +0300

    login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  showPassword: boolean | undefined;
+  userCredentials: any = { userName: '', password: '' };
+  userFormGroup = new FormGroup({
+    userName: new FormControl(),
+    password: new FormControl(),
+  });
   constructor() {}
+  public togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userFormGroup.valueChanges.subscribe((value) => {
+      this.userCredentials = { ...value };
+    });
+  }
+  submitForm(): void {}
 }
--- src/app/components/login/login.module.ts
+++ src/app/components/login/login.module.ts
@@ -1,9 +1,28 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login.routing';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [LoginRoutingModule],
+  imports: [
+    LoginRoutingModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   declarations: [LoginComponent],
   exports: [LoginComponent],
